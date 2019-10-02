0bd7b3f08b991a3610e88eb1842839186390519f
Roman Piskun <sneopik.romka@gmail.com>
2019-10-02 17:14:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+  let res;
+  let i = 0;
+  let j = 0;
+  let arr = [];
+  let ron; //the rest of the number
+  let n;
+
+  first = first.split('').reverse()
+  second = second.split('').reverse()
+  
+  for (i=0;i<second.length;i++)
+  {
+    for (j=0;j<first.length;j++){
+      res = first[j]*second[i];
+      if (arr[i+j])
+        arr[i+j]+=res;
+      else 
+        arr[i+j]=res;
+    }
+  }
+  
+  for (i=0;i<arr.length;i++){
+    n = arr[i]%10;
+    ron = Math.floor(arr[i]/10);
+    arr[i]=n;
+
+    if(arr[i+1])
+      arr[i+1]+=ron;
+    else if (ron!=0)
+      arr.push(ron);
+  }
+
+  arr = arr.reverse().join('');
+
+  return arr;
 }
